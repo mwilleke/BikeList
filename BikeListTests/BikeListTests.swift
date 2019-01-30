73b794d18e3f641b2c68d58524597f2c99182ace
Matthew Willeke <mwilleke@matthews-mbp.lan>
2019-01-29 22:21:08 -0600

    updates

--- BikeListTests/BikeListTests.swift
+++ BikeListTests/BikeListTests.swift
@@ -2,26 +2,24 @@
 //  BikeListTests.swift
 //  BikeListTests
 //
-//  Created by Matthew Willeke on 1/25/19.
+//  Created by Matthew Willeke on 1/29/19.
 //  Copyright © 2019 Matthew Willeke. All rights reserved.
 //
 
 import XCTest
-@testable import BikeList
 
 class BikeListTests: XCTestCase {
 
     override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+       
     }
 
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testLoadData() {
+        
     }
 
     func testPerformanceExample() {
